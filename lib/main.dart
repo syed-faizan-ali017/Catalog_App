59669032306d8fea0cd8487e84e132896a6aaece
Faizan <syed-faizan-ali017>
2023-03-05 00:28:32 +0500

    day3

--- lib/main.dart
+++ lib/main.dart
@@ -1,14 +1,14 @@
 import 'package:flutter/material.dart';
-//import 'package:travel/login.dart';
+import 'package:travel/pages/loginpage.dart';
 import 'package:travel/pages/homepage.dart';
 
-//void main() {
-// runApp(MaterialApp(
-//debugShowCheckedModeBanner: false,
-//   initialRoute: 'login',
+void main() {
+  runApp(MyApp(
+      //debugShowCheckedModeBanner: false,
+      //initialRoute: 'login',
 //  routes: {'login': (context) => Mylogin()},
-// ));
-//}
+      ));
+}
 
 class MyApp extends StatelessWidget {
   @override
@@ -16,6 +16,21 @@ class MyApp extends StatelessWidget {
     return MaterialApp(
       debugShowCheckedModeBanner: false,
       home: Homepage(),
+      themeMode: ThemeMode.light,
+      theme: ThemeData(primarySwatch: Colors.deepOrange),
+      darkTheme: ThemeData(
+        brightness: Brightness.dark,
+      ),
+      // initialRoute: 'loginPage',
+      //routes:(
+      //  {'home': (context) => Homepage()};
+      // {'login': (context) => loginPage()}
+      initialRoute: "/login",
+      routes: {
+        "/": (context) => loginPage(),
+        "/home": (context) => Homepage(),
+        "/login": (context) => LoginPage()
+      },
     );
   }
 }
