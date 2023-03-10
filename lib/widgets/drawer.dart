8d66efd7ce632a3e4902ceb735d2b63107e07c9d
Faizan <syed-faizan-ali017>
2023-03-11 00:17:48 +0500

    day8

---
+++ lib/widgets/drawer.dart
@@ -0,0 +1,87 @@
+import 'package:flutter/cupertino.dart';
+import 'package:flutter/material.dart';
+import 'package:travel/main.dart';
+
+class MyDrawer extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    // FOR IMAGE FROM NETWORK(BROWSER)
+    //Image.url(https://browser image)
+    return Drawer(
+      child: Container(
+        color: Colors.deepPurple,
+        child: ListView(
+          children: [
+            DrawerHeader(
+              padding: EdgeInsets.all(0),
+              child: UserAccountsDrawerHeader(
+                margin: EdgeInsets.all(0),
+                // decoration: BoxDecoration(
+                //   color: Colors.w
+                //),
+                accountName: Text("Faizan Ali"),
+                accountEmail: Text("decor@gmail.com"),
+                currentAccountPicture: CircleAvatar(
+                    backgroundImage: AssetImage("assets/images/faizan.jpeg")),
+
+                //currentAccountPicture: Image.network(Image.url),
+              ),
+            ),
+            ListTile(
+              leading: Icon(
+                CupertinoIcons.home,
+                color: Colors.white,
+              ),
+              title: Text(
+                "Home",
+                textScaleFactor: 1.2,
+                style: TextStyle(
+                  color: Colors.white,
+                ),
+              ),
+            ),
+            ListTile(
+              leading: Icon(
+                CupertinoIcons.profile_circled,
+                color: Colors.white,
+              ),
+              title: Text(
+                "Profile",
+                textScaleFactor: 1.2,
+                style: TextStyle(
+                  color: Colors.white,
+                ),
+              ),
+            ),
+            ListTile(
+              leading: Icon(
+                CupertinoIcons.mail,
+                color: Colors.white,
+              ),
+              title: Text(
+                "Email",
+                textScaleFactor: 1.2,
+                style: TextStyle(
+                  color: Colors.white,
+                ),
+              ),
+            ),
+            ListTile(
+              leading: Icon(
+                CupertinoIcons.phone,
+                color: Colors.white,
+              ),
+              title: Text(
+                "Contact",
+                textScaleFactor: 1.2,
+                style: TextStyle(
+                  color: Colors.white,
+                ),
+              ),
+            ),
+          ],
+        ),
+      ),
+    );
+  }
+}
