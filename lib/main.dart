c4959e15db1d2589a3c6283b9f08773f9316e183
Faizan <syed-faizan-ali017>
2023-03-11 00:54:49 +0500

    Day9

--- lib/main.dart
+++ lib/main.dart
@@ -1,8 +1,9 @@
 import 'package:flutter/material.dart';
 import 'package:travel/pages/loginpage.dart';
 import 'package:travel/pages/homepage.dart';
-//import 'package:google_fonts/google_fonts.dart';
+
 import 'package:travel/utilis/routes.dart';
+import 'package:travel/widgets/Theme.dart';
 
 void main() {
   runApp(MyApp(
@@ -18,17 +19,12 @@ class MyApp extends StatelessWidget {
       debugShowCheckedModeBanner: false,
       home: Homepage(),
       themeMode: ThemeMode.light,
-      theme: ThemeData(
-        primarySwatch: Colors.deepPurple,
-        //fontFamily: GoogleFonts.lato().fontFamily,
-        //primaryTextTheme: GoogleFonts.latoTextTheme()
-      ),
-      darkTheme: ThemeData(
-        brightness: Brightness.dark,
-      ),
-      initialRoute: "login",
+      theme: MyTheme.lightTheme(context),
+      darkTheme: MyTheme.darkTheme(context),
+      initialRoute: MyRoutes.homeRoute,
       routes: {
-        "login": (context) => LoginPage(),
+        // "login": (context) => LoginPage(),
+        MyRoutes.homeRoute: (context) => Homepage(),
         MyRoutes.loginRoute: (context) => LoginPage(),
         MyRoutes.homeRoute: (context) => Homepage(),
       },
--- lib/pages/homepage.dart
+++ lib/pages/homepage.dart
@@ -1,26 +1,19 @@
 import 'package:flutter/material.dart';
+import 'package:travel/widgets/drawer.dart';
 
 class Homepage extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
-        title: Text("Catalog App"),
+        title: Text("Catalog App", textAlign: TextAlign.center),
       ),
       body: Center(
         child: Container(
           child: Text("Welcome to 30 days of flutter"),
         ),
       ),
-      drawer: Drawer(
-        child: Text("Sign in",
-            style: TextStyle(
-              color: Colors.white,
-              fontWeight: FontWeight.bold,
-              fontSize: 18,
-              backgroundColor: Colors.deepPurple,
-            )),
-      ),
+      drawer: MyDrawer(),
     );
   }
 }
